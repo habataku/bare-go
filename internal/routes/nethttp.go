d0e3dd8bac5f402e83dc2a2bec89a15a92a4a529
MotorTruck1221 <tuckerj0606@icloud.com>
2023-11-29 09:23:32 -0700

    CORS needs to be handled by user when using NET/http

--- internal/routes/nethttp.go
+++ internal/routes/nethttp.go
@@ -3,20 +3,20 @@ package routes
 
 import (
     "net/http"
-    "github.com/rs/cors"
+    //"github.com/rs/cors"
     //imports from project
     "github.com/ruby-network/bare-go/internal/utils"
     "github.com/ruby-network/bare-go/internal/v3"
 )
 
 func NetHttp(directory string, router *http.ServeMux) *http.ServeMux {
-    cors := cors.New(cors.Options{
-        AllowedOrigins: []string{"*"},
-        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-        AllowedHeaders: []string{"*"},
-        ExposedHeaders: []string{"*"},
-        MaxAge: 300,
-    })
+    //cors := cors.New(cors.Options{
+       // AllowedOrigins: []string{"*"},
+       // AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+       // AllowedHeaders: []string{"*"},
+       // ExposedHeaders: []string{"*"},
+       // MaxAge: 300,
+    //})
 
     router.HandleFunc(directory, func(w http.ResponseWriter, r *http.Request) {
         e := utils.GetJson()
